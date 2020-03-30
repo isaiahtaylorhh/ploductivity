e6c8f792401e5670f2515c717cf50fb70f998450
isaiahtaylor <isaiah@hoodhub.com>
2020-03-29 23:30:12 -0700

    documented jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,7 @@
+/*
+ * followed option 2 of https://github.com/react-native-community/async-storage/blob/LEGACY/docs/Jest-integration.md
+ */
+
 const jest = require('jest');
 
 const mockAsyncStorage = require('@react-native-community/async-storage/jest/async-storage-mock');
