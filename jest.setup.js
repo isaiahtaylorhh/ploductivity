198629e280e04c30c7c66bd3682cea3ccd8fcc4c
isaiahtaylor <isaiah@hoodhub.com>
2020-03-29 23:25:35 -0700

    fixed jest with async storage

---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+const jest = require('jest');
+
+const mockAsyncStorage = require('@react-native-community/async-storage/jest/async-storage-mock');
+
+jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
